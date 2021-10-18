70ee2265eb71fd59af32d2918337c144055dee08
Stanislav <34060681+bazuzzu@users.noreply.github.com>
2021-10-18 17:17:05 +0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -18,8 +18,9 @@ export default class MyDocument extends Document {
             href="/assets/css/swipeslider.css"
             rel="stylesheet"
           />	
-		<link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Jost" />
+		<link rel="preconnect" href="https://fonts.googleapis.com"/>
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link href="https://fonts.googleapis.com/css2?family=Jost:wght@200&display=swap" rel="stylesheet" />
 		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 		<script src="/assets/js/script.js"></script>
 		<script src="/assets/js/jquery-ui.min.js"></script>
